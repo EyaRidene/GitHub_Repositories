45fad7f4e2264f479e5f8848323a46505c808dfa
EyaRidene <eya.ridene@insat.ucar.tn>
2024-08-02 20:50:53 +0100

    Add location email linkedin and followers to the profile details

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -16,7 +16,13 @@ interface ProfileProps {
 const Profile: React.FC<ProfileProps> = ({
   avatarUrl,
   name,
-  bio
+  bio,
+  location,
+  publicRepos,
+  followers,
+  following,
+  email,
+  linkedin
 }) => {
   return (
     <div className="profile">
@@ -25,6 +31,10 @@ const Profile: React.FC<ProfileProps> = ({
       <h3 className="profile-name-second">{name}</h3>
       <p className="profile-bio">{bio}</p>
       <button className='follow-button'>Follow</button>
+      {location && <p className="profile-location"><i className="fa-solid fa-location-dot"></i> {location}</p>}
+      <p className="profile-followers"><i className="fa-solid fa-person"></i><span>{followers}</span> followers. <span>{following}</span> following</p>
+      {email && <p className="profile-email"><i className="fa-solid fa-envelope"></i>Email: {email}</p>}
+      {linkedin && <p className="profile-linkedin"><i className="fa-brands fa-linkedin"></i><a href={linkedin} target="_blank" rel="noopener noreferrer">{linkedin}</a></p>}
     </div>
   );
 };
